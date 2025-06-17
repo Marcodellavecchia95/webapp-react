2ad23010bf57fb845d9a586102040361378ed742
Marcodellavecchia95 <mdellavecchia95@gmail.com>
2025-06-17 17:56:06 +0200

    fix card component

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,14 +1,14 @@
 import { Link } from "react-router-dom";
 
-export default function MovieCard({ id, title, director, image }) {
+export default function MovieCard({ id, title, abstract, image }) {
   return (
     <div className="col-4">
       <div className="card mt-3">
-        <img src={image} className="card-img-top" alt="" />
+        {image && <img src={image} className="card-img-top" alt="" />}
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
-
-          <Link to="/:id" className="btn btn-primary">
+          {abstract && <p className="card-text">{abstract}</p>}
+          <Link to={`/:${id}`} className="btn btn-primary">
             Vai alle recensioni
           </Link>
         </div>
