e0297cac63a18298b0386cb07e8cf97f8d47ebd3
Marcodellavecchia95 <mdellavecchia95@gmail.com>
2025-06-18 14:37:09 +0200

    add axios and movies map

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,26 @@
+import { useEffect, useState } from "react";
 import MovieCard from "../components/MovieCard";
+import axios from "axios";
+const ApiBackend = import.meta.env.VITE_BACKEND_URL;
 
 export default function HomePage() {
+  const [movies, setMovies] = useState([]);
+  const fetchMovies = () => {
+    axios.get(ApiBackend).then((res) => {
+      setMovies(res.data);
+      console.log(movies);
+    });
+  };
+
+  useEffect(fetchMovies, []);
+
   return (
     <main>
       <div className="container">
         <div className="row">
-          <MovieCard />
+          {movies.map((movie) => {
+            return <MovieCard />;
+          })}
         </div>
       </div>
     </main>
